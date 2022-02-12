03a68ea891f9b772f5cd95c5b119035e0d1b1334
Walteann <walteann3@gmail.com>
2022-02-11 21:21:45 -0300

    feat: comeco sidebar

---
+++ src/app/core/layouts/layouts-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  imports: [],
+  exports: [SidebarComponent],
+  declarations: [SidebarComponent],
+  providers: [],
+})
+export class LayoutModule { }
