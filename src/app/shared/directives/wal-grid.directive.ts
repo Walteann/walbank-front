9046e03517475cf482accfa5225a583d111985ea
Walteann <walteann3@gmail.com>
2022-02-20 02:54:13 -0300

    feat: upgrade diretiva walGrid para fracional

--- src/app/shared/directives/wal-grid.directive.ts
+++ src/app/shared/directives/wal-grid.directive.ts
@@ -3,9 +3,9 @@ import { Directive, ElementRef, Input } from '@angular/core';
 @Directive({ selector: '[walGrid]' })
 export class WalGridDirective {
 
-	@Input() set walGrid(value: number) {
+	@Input() set walGrid(value: number | string ) {
 		this.el.nativeElement.style.display = 'grid';
-		this.el.nativeElement.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+		this.el.nativeElement.style.gridTemplateColumns = typeof value === 'number' ? `repeat(${value}, 1fr)` : `${value}`;
 	}
 	constructor(private el: ElementRef) {}
 }
