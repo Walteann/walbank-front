7d10060cec3ccb10d4d457739a3bbf846b105dab
Walteann <walteann3@gmail.com>
2022-03-08 20:26:35 -0300

    feat: adicionardo breadcrumb no approuting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,23 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 	{
 		path: 'home',
-		loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+		loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
+		data: { breadcrumb: 'Home' }
 	},
 	{
 		path: 'payments',
-		loadChildren: () => import('./pages/payments/payments.module').then(m => m.PaymentsModule)
+		loadChildren: () => import('./pages/payments/payments.module').then(m => m.PaymentsModule),
+		data: { breadcrumb: 'Pagamentos' }
 	},
 	{
 		path: 'tickets',
-		loadChildren: () => import('./pages/tickets/tickets.module').then(m => m.TicketsModule)
+		loadChildren: () => import('./pages/tickets/tickets.module').then(m => m.TicketsModule),
+		data: { breadcrumb: 'Boletos' }
 	},
 	{
 		path: 'settings',
-		loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule)
+		loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule),
+		data: { breadcrumb: 'Configuração' }
 	},
 	{
 		path: '',
