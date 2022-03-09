63adcd63602af772eea30f38ff747143206edaf4
Walteann <walteann3@gmail.com>
2022-03-08 21:08:43 -0300

    feat: filtro boleto

--- src/app/pages/tickets/tickets.component.ts
+++ src/app/pages/tickets/tickets.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-tickets',
-  templateUrl: './tickets.component.html',
-  styleUrls: ['./tickets.component.scss']
+	selector: 'app-tickets',
+	templateUrl: './tickets.component.html',
+	styleUrls: ['./tickets.component.scss'],
 })
-export class TicketsComponent implements OnInit {
+export class TicketsComponent {
 
-  constructor() { }
+	categories = [
+		{
+			label: 'Educação',
+			value: 1
+		},
+		{
+			label: 'Saude',
+			value: 2
+		},
+		{
+			label: 'Supermercado',
+			value: 3
+		},
+		{
+			label: 'Entreterimento',
+			value: 4
+		}
+		// TODO: Criar um banco de dados com essas categorias
+	]
 
-  ngOnInit(): void {
-  }
+	formFilter: FormGroup;
 
+	constructor(
+		formBuilder: FormBuilder
+	) {
+		this.builderFormFilter(formBuilder);
+	}
+
+	onSubmit(): void {
+		// TODO: Continuar aqui
+	}
+
+	private builderFormFilter(formBuilder: FormBuilder): void {
+		this.formFilter = formBuilder.group({
+			description: [null],
+			amount: [null],
+			category: [null],
+			dueDate: [null]
+		});
+		// TODO: Adicionar Validação
+	}
 }
+
--- src/app/pages/tickets/tickets.module.ts
+++ src/app/pages/tickets/tickets.module.ts
@@ -1,12 +1,14 @@
-import { WalHeaderInfoModule } from './../../shared/components/header-info/wal-header-info.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
+import { WalHeaderNavModule } from 'src/app/core/layouts/header-nav/header-nav.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
-import { TicketsComponent } from './tickets.component';
+import { WalHeaderInfoModule } from '../../shared/components/header-info/wal-header-info.module';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
-import { WalHeaderNavModule } from 'src/app/core/layouts/header-nav/header-nav.module';
+import { TicketsComponent } from './tickets.component';
 
 const routes = [
 	{
@@ -26,6 +28,8 @@ const routes = [
 	imports: [
 		CommonModule,
 		RouterModule.forChild(routes),
+		FormsModule,
+		ReactiveFormsModule,
 		SharedModule,
 		WalHeaderInfoModule,
 		WalHeaderNavModule
