233eee0157dd260963b5a74063219c512136b115
Walteann <walteann3@gmail.com>
2026-01-21 22:00:28 -0300

    feat: update version angular 15 to 16

--- src/app/pages/debtors/debtors-resolver.service.ts
+++ src/app/pages/debtors/debtors-resolver.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { catchError, Observable, of } from 'rxjs';
 import { DebtorsService } from 'src/app/core/http/debtors.service';
 import { Debtor } from 'src/app/shared/model';
 
 
 @Injectable({providedIn: 'root'})
-export class DebtorsResolverService implements Resolve<Debtor> {
+export class DebtorsResolverService  {
 
 	constructor(private debtorsService: DebtorsService, private router: Router) { }
 
